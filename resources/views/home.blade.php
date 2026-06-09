7715f6892af220080bb6372068a0c7ec72791c58
李泽峰 <375351298@qq.com>
2026-06-10 02:42:51 +0800

    feat: 主页添加下载原神和鸣潮的链接按钮（蜗牛大了）

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,4 +5,12 @@
 @section('content')
     <h2>欢迎你，{{ Auth::user()->username }}！登录成功</h2>
     <p>这是系统主页，功能可扩展。</p>
+    <br>
+    <a href="https://ys.mihoyo.com/" target="_blank">
+        <button>点我下载原神</button>
+    </a>
+    &nbsp;&nbsp;
+    <a href="https://mc.kurogames.com/" target="_blank">
+        <button>点我下载鸣潮</button>
+    </a>
 @endsection
